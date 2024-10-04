ec00bd3f3ed7336185dfa67adc80f144b17976c7
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-04 08:28:43 +0530

    git hub

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+def makeCommits(days: int):
+    if days < 1:
+        os.system('git push')
+        return 0  # Return a value (0) when pushing is done
+    else:
+        dates = f"{days} days ago"
+        with open('data.txt', 'a') as file:
+            file.write(f'{dates} <- this was the commit for the day!!\n')
+        
+        # staging 
+        os.system('git add data.txt')
+
+        # commit 
+        os.system('git commit --date="'+ dates +'" -m "First commit for the day!"')
+
+        # Continue with the next day
+        makeCommits(days - 10)  # No need to multiply, just call recursively
+
+# Call the function
+makeCommits(20)
